91431c9d6cc32f97e46b3149f65070b498822209
MathieuRobilliard <mathieu.robilliard@free.fr>
2016-04-06 12:10:18 +0200

    Add getter for contents in Vertex

--- Tp6-Graph Project/src/graph/Vertex.java
+++ Tp6-Graph Project/src/graph/Vertex.java
@@ -11,13 +11,24 @@ public class Vertex {
 	 * A general contents
 	 */
 	private Object contents;
-	
+
 	/**
 	 * Make a vertex with an contents
 	 * @param contents The contents to save in the vertex
 	 */
 	public Vertex(Object contents) {
 		this.contents = contents;
+	}
+	
+	/**
+	 * @return The contents of the vertex
+	 */
+	public Object getContents() {
+		return contents;
+	}
+	
+	public String toString() {
+		return "" + contents;
 	}
 
 }
